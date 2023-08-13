e7332a9b7e88ef6ccb9c4943bd49215308c58da5
Menna Hassan <h.menna85@gmail.com>
2023-08-13 10:49:09 -0700

    file_storage added

---
+++ models/engine/file_storage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+""" File storage model """
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class FileStorage:
+    """Defines a storage engine for Hbnb project.
+    """
+
+    __file_path = 'file.json'
+    __objects = {}
+    class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                  "Amenity": Amenity, "City": City, "Review": Review,
+                  "State": State}
+
+    def all(self):
+        '''Return dictionary of class instance'''
+        return self.__objects
+
+    def new(self, obj):
+        '''create new obj to existing dictionary of instances'''
+        if obj:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the json file"""
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """deserializes the Json file to dict"""
+        try:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                new_obj_dict = json.load(f)
+            for key, value in new_obj_dict.items():
+                class_name = value.get('__class__')
+                obj_class = self.class_dict.get(class_name)
+                if obj_class:
+                    obj = obj_class(**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
